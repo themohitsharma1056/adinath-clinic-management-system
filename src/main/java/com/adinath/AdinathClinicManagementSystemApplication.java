23d535b9bd1d833e4fe1cc17d64d22031743b11c
mohit sharma <the.mohit1056@gmail.com>
2026-06-04 15:49:59 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/adinath/AdinathClinicManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.adinath;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdinathClinicManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdinathClinicManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adinath/AdinathClinicManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adinath;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdinathClinicManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
